2f1eb35f10d356356ef41b70297b294867c914d0
ReisSalv07 <gabrielreis200509@gmail.com>
2026-03-25 21:58:59 -0300

    Create Calculadora.java

---
+++ Aula3/exemplo2/Calculadora.java
@@ -0,0 +1,18 @@
+package exemplo2;
+
+public class Calculadora {
+    int n1,n2,total;
+
+    public void subtracao() {
+        total = n1 - n2;
+    }
+    public void adicao() {
+        total = n1 + n2;
+    }
+    public void multiplicacao() {
+        total = n1  * n2;
+    }
+    public void divisao() {
+        total = n1 / n2;
+    }
+}
